8151dd784285abfe2a7792f4526e52afef6fc0b5
charly vdn <charly54vdn@gmail.com>
2025-05-17 14:52:36 +0200

    correction de la configuration pour vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/formation-code-ia/', // Remplacez par le nom de votre dépôt GitHub
+  base: '/', // C'est le chemin par défaut pour Vercel
 })
